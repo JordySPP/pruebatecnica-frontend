e243658d13bd27393e20da4027a2d816f4af1825
JordySPP <157540504+JordySPP@users.noreply.github.com>
2026-03-08 23:21:38 -0600

    Cuarto commit
    Ajustes

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -7,11 +7,11 @@ import Reportes from "../pages/Reportes";
 function AppRoutes() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/motoristas" />} />
       <Route path="/motoristas" element={<Motoristas />} />
       <Route path="/vehiculos" element={<Vehiculos />} />
-      <Route path="/Registros" element={<Registros />} />
+      <Route path="/registros" element={<Registros />} />
       <Route path="/reportes" element={<Reportes />} />
-      <Route path="/" element={<h1>Home</h1>} />
     </Routes>
   );
 }
